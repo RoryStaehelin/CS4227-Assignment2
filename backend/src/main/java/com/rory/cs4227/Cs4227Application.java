710978ae593d09f3114538d0ee27f9f85becb9d2
Rory Staehelin <rostaehelin@gmail.com>
2025-03-01 18:50:38 +0000

    Initial Commit

---
+++ backend/src/main/java/com/rory/cs4227/Cs4227Application.java
@@ -0,0 +1,13 @@
+package com.rory.cs4227;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4227Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4227Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rory/cs4227/Cs4227ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rory.cs4227;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4227ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
