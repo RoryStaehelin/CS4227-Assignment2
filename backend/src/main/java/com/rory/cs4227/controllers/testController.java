7e80928fb963dac3a480f7730f6ce1595b864683
Rory Staehelin <rostaehelin@gmail.com>
2025-03-11 16:04:55 +0000

    Backend folder structure

--- backend/src/main/java/com/rory/cs4227/controllers/testController.java
+++ backend/src/main/java/com/rory/cs4227/controllers/testController.java
@@ -1,4 +1,4 @@
-package com.rory.cs4227.controller;
+package com.rory.cs4227.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
