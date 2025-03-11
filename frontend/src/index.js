0a59b2d934daf3bae0e6a3f38c152c4023b371d3
Rory Staehelin <rostaehelin@gmail.com>
2025-03-11 17:28:05 +0000

    Fix for github.io deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
 );
