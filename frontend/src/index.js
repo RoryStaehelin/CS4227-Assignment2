e77f199bc070d7e6a4dd1ba7c3dcdfa76c2efe77
Rory Staehelin <rostaehelin@gmail.com>
2025-03-14 16:25:28 +0000

    Fixed index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </Provider>,
